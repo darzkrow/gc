714c74f0a9a513040862911ffe2bc3f9f53a4e92
DarzKrow <gfrancojf@gmail.com>
2024-06-11 11:34:55 -0400

    dnb

--- Employes/models.py
+++ Employes/models.py
@@ -18,17 +18,15 @@ class Estados(models.Model):
         db_table = 'estados'
 
 class Employes(models.Model):
-    status = models.CharField('Status', choices=Status_ordenadas, max_length=3, null=True, blank=True
-                              
-                              
-                              )
+    status = models.CharField('Status', choices=Status_ordenadas, max_length=3, null=True, blank=True)
     cedper = models.CharField('cedula', max_length=9, primary_key=True,validators=[MinLengthValidator(6)])
     nomper = models.CharField('Nombre', max_length=40,validators=[MinLengthValidator(3)])
     apeper = models.CharField('Apellido', max_length=40,validators=[MinLengthValidator(3)])
     telmovper = PhoneNumberField('Telf. Movil',null=True, blank=True, max_length=13)
     dependencia = models.CharField('Ubicacion Admistración', choices=Dependencias_ordenadas, max_length=255)
     oficina = models.CharField('Oficina', choices=Oficinas_ordenadas, max_length=10)
-    cargo = models.CharField('Cargo', choices=Cargos_ordenadas, max_length=10, default='DN')
+    cargo = models.CharField('Cargo', choices=Cargos_ordenadas, max_length=255, default='DN')
+
     estado = models.ForeignKey(Estados, verbose_name='Estado', on_delete=models.CASCADE)
     municipio = models.CharField('Municipio', max_length=30)
     parroquia = models.CharField('Parroquia', max_length=30)
